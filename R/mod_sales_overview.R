9a5021fe81d8b541eebbfae285c4c253b97dbb36
Kelly Loucks <kelly@stoltzmanconsulting.com>
2021-02-03 16:36:55 -0700

    Add slider input and shiny test necessities

--- R/mod_sales_overview.R
+++ R/mod_sales_overview.R
@@ -10,20 +10,29 @@
 mod_sales_overview_ui <- function(id){
   ns <- NS(id)
   tagList(
-shinydashboard::box(
-  width = 12,
   fluidRow(
-    column(
-      width = 6,
-      DT::DTOutput(ns("table_example"))
-    ),
-    column(
+    width = 12,
+    shinydashboard::box(
     width = 6,
+    sliderInput(ns("obs_count"), "# of Observations",
+                min = 5, max = 75,
+                value = 10)
+      
+    )
+  ),
+  fluidRow(
+    width = 12,
+    shinydashboard::box(
+      width = 7,
+      DT::DTOutput(ns("table_example"))
+        ),
+  shinydashboard::box(
+    width = 5,
     plotOutput(ns("plot_example"))
-  )
+    )
   )
   ) 
-  )
+
 }
     
 #' sales_overview Server Function
@@ -33,7 +42,7 @@ mod_sales_overview_server <- function(input, output, session){
   ns <- session$ns
  
   output$table_example <- DT::renderDT(
-    shinipsum::random_DT(nrow = 5, ncol = 6)
+    shinipsum::random_DT(nrow = input$obs_count, ncol = 4)
   )
   
   output$plot_example <- renderPlot(
